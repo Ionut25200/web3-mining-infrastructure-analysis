1c8b86a258c575bd6ddc9ae2e273c81319a9996a
Ionut25200 <contact.beezeweb@gmail.com>
2026-05-07 18:22:35 +0200

    Add daily revenue calculation function

--- data/main.py
+++ data/main.py
@@ -1,1 +1,16 @@
+def calculate_daily_revenue(hashrate_th, sta_per_th):
+
+  """
+  calculates estimated daily revenue in Satoshis.
+  Target : 105.75 TH/s @ sta/TH
+  """
+  revenue = hasrate_th * sat_per_th
+  return revenue
+
+my hashrate = 104
+efficiency_yield = 7
+
+daily_total = calculated_daily_revenue(my_hashrate, efficiency_yield)
+print(f"Current Daily Yield: {daily_total} Satoshis")
+
 
